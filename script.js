5971d67fa73887c45f3982fc57093fb2253d693f
ishahtech <salawudeenaishatolabisi@gmail.com>
2024-05-25 23:54:42 +0100

    fix: comments added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//declaration to get the inner html
 const inputField = document.getElementById('input');
 const displayResultField = document.getElementById('result-display');
 
@@ -6,26 +7,27 @@ function appendValue (value) {
     inputField.value += value;
 }
 
-//function to delete entered input
+//function to clear all input
 function clearInput () {
     inputField.value = '';
     displayResultField.textContent = '';
 }
 
+//function to delete an input
 function deleteLastInput () {
     inputField.value = inputField.value.slice(0, -1);
 }
 
-
+//function that triggers the '=' event
 function calculateResult() {
     try {
-        const result = eval(inputField.value); // eval is used for simplicity but not recommended for production
+        const result = eval(inputField.value); // eval is used to denote evaluation
         displayResultField.textContent = result;
     } catch (error) {
         displayResultField.textContent = 'Error';
     }
 }
-
+//function to perform percentage
 function handlePercentage() {
     try {
         const result = eval(inputField.value) / 100;
